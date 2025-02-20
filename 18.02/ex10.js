37b44f5b2abfbfa1c1c588006a791e8dc738fd31
Lucas <137101703+LucasMoras@users.noreply.github.com>
2025-02-20 14:43:30 -0300

    Update ex10.js

--- 18.02/ex10.js
+++ 18.02/ex10.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 
 rl.question("Digite seu salario: ", (salario) => {
 
-const aumento = parseFloat(salario) * 0.15;
+const aumento = parseFloat(salario) * 1.15;
 console.log("Salario final: " + aumento);
 
 rl.close();
